961edc2c0b9fe553288c6f9c84b9271e9d813793
Carolina Nonato <csnonato@gmail.com>
2023-02-15 10:35:27 -0500

    setting the db

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import Title from "./components/Title";
+import {
+  useState, useEffect
+} from "react";
 
 const App = () => {
+
+
+  const [quiz, setQuiz] = useState(false)
+
+  const fetchData = async () => {
+
+    try {
+      const response = await fetch('http://localhost:8000/quiz');
+      const json = await response.json();
+      console.log(json);
+      setQuiz(json);
+    } catch (error) {
+      console.log(error);
+    }
+
+
+  }
+
+  useEffect(() => { fetchData() }, [])
+
   return (
     <div>
       <Title />
